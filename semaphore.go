05f763f649c850cd71ec8b5ab77175216e38e963
pavel-kazhavets <pavel.kazhavets@kufar.by>
2016-07-24 07:13:14 +0300

    Add documentation

--- semaphore.go
+++ semaphore.go
@@ -1,28 +1,38 @@
+// Package syncx is intended to provide functionality additional to stdlib sync package.
 package syncx
 
 import (
 	"errors"
 )
 
+// Simple semaphore not visible from outside of programm.
+// It is implemented using golang channels. Functions related to it
+// (like Aquire, Release, etc.) will simply block till they will be able to
+// perform action on semaphore.
 type Semaphore struct {
 	body chan struct{}
 }
 
+// NewSemaphore function accepts single argument - expected capacity as integer and
+// returns pointer to created semaphore.
 func NewSemaphore(capacity int) *Semaphore {
 	sem := new(Semaphore)
 	sem.body = make(chan struct{}, capacity)
 	return sem
 }
 
+// Aquire function increments semaphore.
 func (sem *Semaphore) Aquire() {
 	entry := struct{}{}
 	sem.body <- entry
 }
 
+// Release function decrements semaphore.
 func (sem *Semaphore) Release() {
 	<-sem.body
 }
 
+// AquireN function increments semaphore by N. It will return error if N > cap(sem).
 func (sem *Semaphore) AquireN(n int) (err error) {
 	if cap(sem.body) < n {
 		err = errors.New("Capacity of semaphore is less than aquired value with AquireN.")
@@ -36,6 +46,7 @@ func (sem *Semaphore) AquireN(n int) (err error) {
 	return err
 }
 
+// ReleaseN function decrements semaphore by N. It will return error if N > cap(sem).
 func (sem *Semaphore) ReleaseN(n int) (err error) {
 	if cap(sem.body) < n {
 		err = errors.New("Capacity of semaphore is less than requested value for release.")
@@ -49,12 +60,14 @@ func (sem *Semaphore) ReleaseN(n int) (err error) {
 	return err
 }
 
+// AquireAll function increments semaphore till its maximum capacity.
 func (sem *Semaphore) AquireAll() {
 	for i := 0; i < cap(sem.body); i++ {
 		sem.Aquire()
 	}
 }
 
+// ReleaseAll function decrements semaphore till its maximum capacity.
 func (sem *Semaphore) ReleaseAll() {
 	for i := 0; i < cap(sem.body); i++ {
 		sem.Release()
